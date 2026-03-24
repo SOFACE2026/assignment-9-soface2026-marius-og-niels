73c62c2d607d444d98f9a614d5b7684c36ff4a8e
Marius Ulrich Vase <mariusulrichvase@gmail.com>
2026-03-24 14:58:31 +0100

    Co-authored-by: Niels Christian Perregaard Thorsen <NielsThorsen@users.noreply.github.com>

--- src/sedan.cpp
+++ src/sedan.cpp
@@ -5,32 +5,39 @@ static const std::array<double, 7> gear_ratios = {-1, 1, 2, 3, 4, 5, 6};
 
 Sedan::Sedan() : current_gear(0)
 {
+    
 }
 
 void Sedan::shift_up()
 {
+    if (current_gear < 6) {
+        current_gear++;
+    }
 }
 
 void Sedan::shift_down()
 {
+    if (current_gear > 0){
+        current_gear--;
+    }
 }
 
 double Sedan::get_torque(double engine_rpm, double throttle)
 {
-    return 0;
+    return engine_rpm * throttle * 100;
 }
 
 double Sedan::get_weight()
 {
-    return 1;
+    return 1500;
 }
 
 double Sedan::get_wheel_radius()
 {
-    return 1;
+    return 0.3;
 }
 
 double Sedan::get_current_gear_ratio()
 {
-    return 1;
+    return gear_ratios[current_gear];
 }
